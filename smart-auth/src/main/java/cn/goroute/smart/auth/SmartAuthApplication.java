1ffb18d05b50560eb58a703c07ec1cada76ab089
Alickx <alickx@foxmail.com>
2022-10-15 01:22:01 +0800

    删除无用文件，设置Feign扫描位置

--- smart-auth/src/main/java/cn/goroute/smart/auth/SmartAuthApplication.java
+++ smart-auth/src/main/java/cn/goroute/smart/auth/SmartAuthApplication.java
@@ -1,12 +1,10 @@
 package cn.goroute.smart.auth;
 
-import com.hccake.ballcat.common.swagger.annotation.EnableSwagger2Provider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
@@ -15,7 +13,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @SpringBootApplication(scanBasePackages = "cn.goroute.smart")
 @EnableDiscoveryClient
 @MapperScan("cn.goroute.smart.*.mapper")
-@EnableFeignClients
+@EnableFeignClients(basePackages = "cn.goroute.smart.*.feign")
 @EnableOpenApi
 public class SmartAuthApplication {
 
